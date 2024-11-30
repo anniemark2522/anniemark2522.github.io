ba004344064195c94bfacf688d72a2706bf38aa8
anniemark2522 <113179306+anniemark2522@users.noreply.github.com>
2024-11-30 13:33:39 +0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-subject=['datacom','os','comarch']
-for subname in subject:
-    sumscore=0
-    print(subname)
-    for i in range(1,4,1):
-        score=int(input("Enter your score :"))
-        sumscore=sumscore+score
-    print(sumscore)
